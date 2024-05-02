690202de00fbbdf7b2d78369b2ffae1b23acf648
Matt Lohier <30995440+MattJLohier@users.noreply.github.com>
2024-05-02 22:39:56 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ import requests
 import matplotlib.pyplot as plt
 from streamlit_echarts import st_echarts
 import pytz
-import openpyxl
 
 # URL of the image you want to use as the page icon
 icon_url = "https://i.postimg.cc/Y0XLcpg7/scooper-s.png"
