271c9908c1fab95de4423b6863115b7655a7422e
Matt Lohier <30995440+MattJLohier@users.noreply.github.com>
2024-05-17 00:18:34 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -292,11 +292,11 @@ def dcr_report():
 
                 df_mapping = pd.read_excel(file_mapping)
                 
-                st.write(df_report)
+                
                 # Merge file_mapping into file_pivot on the 'Product' column
                 df_pivot = pd.merge(df_pivot, df_mapping, on='Product', how='left')
                 df_report = pd.merge(df_report, df_mapping, on='Product', how='left')
-
+                st.write(df_report)
                 merged_file = "merged_pivot.xlsx"
                 with pd.ExcelWriter(merged_file) as writer:
                     df_pivot.to_excel(writer, sheet_name="Product & Pricing Pivot Data", index=False)
