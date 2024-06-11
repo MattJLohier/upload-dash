c34b4f0263640e0203743c7766dac4059f09bde2
Matt Lohier <30995440+MattJLohier@users.noreply.github.com>
2024-06-11 03:16:16 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -316,7 +316,8 @@ def dcr_report():
         file3 = st.file_uploader("Upload A DCR File", type=["xlsx"], key='first_file_uploader')
         file2 = st.file_uploader("Upload Specs; EU TCO or US P&P ", type=["xlsx"], key='second_file_uploader')
         # Using HTML to change the color of the text
-        st.markdown('<p style="color: purple;">Upload the UID Mapping File</p>', unsafe_allow_html=True)
+        # Using HTML to change only the "UID Mapping File" text to purple without margin or padding
+        st.markdown('<label><span style="color: purple; margin: 0; padding: 0;">Upload the UID Mapping File</span></label>', unsafe_allow_html=True)
         file4 = st.file_uploader("", type=["xlsx"])
         process_button = st.button("Process and Upload to S3", key='key1', disabled=not (file3 and file2))
 
