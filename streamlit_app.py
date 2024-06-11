3d1e76c324c477bacfb292c6dbd10682f3d12d5f
Matt Lohier <30995440+MattJLohier@users.noreply.github.com>
2024-06-11 06:03:09 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -177,7 +177,7 @@ def main():
 
         # Show modal when the button is clicked
         if st.session_state['show_modal']:
-            with st.sidebar.expander("Profile", expanded=True):
+            with st.sidebar:
                 st.write("Profile details go here.")
                 # Add other profile details or components as needed
                 if st.button("Close", key='close_modal'):
